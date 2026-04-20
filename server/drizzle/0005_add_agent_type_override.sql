10ecefc1fcf272dc91ca9089a754fdbaf8d22482
buchananwill <139961977+buchananwill@users.noreply.github.com>
2026-04-20 10:12:56 +0100

    Add statement breakpoint.

--- server/drizzle/0005_add_agent_type_override.sql
+++ server/drizzle/0005_add_agent_type_override.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "tasks" ADD COLUMN "agent_type_override" text;
+--> statement-breakpoint
 ALTER TABLE "tasks" ADD CONSTRAINT "tasks_agent_type_override_check" CHECK ("agent_type_override" IS NULL OR "agent_type_override" ~ '^[a-zA-Z0-9_-]{1,64}$');
